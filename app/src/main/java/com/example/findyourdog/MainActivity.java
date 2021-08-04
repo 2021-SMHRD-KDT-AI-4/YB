c6858a6822c94ee6df9fdf85ee260acb9dc354ba
muyaho <lch980628@gmail.com>
2021-08-04 16:38:31 +0900

    무야호~1

--- app/src/main/java/com/example/findyourdog/MainActivity.java
+++ app/src/main/java/com/example/findyourdog/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 무야호
+    // 무야호12222
     // 전연지
     @Override
     protected void onCreate(Bundle savedInstanceState) {
