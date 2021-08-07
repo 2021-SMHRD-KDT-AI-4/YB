ad8a71b039f5236534c3a64078810b5dc19984c2
muyaho <lch980628@gmail.com>
2021-08-07 16:14:57 +0900

    자주하는 질문 비동기식으론 못했지만 아무튼 했음

--- app/src/main/java/com/example/findyourdog/Question.java
+++ app/src/main/java/com/example/findyourdog/Question.java
@@ -1,36 +1,68 @@
 package com.example.findyourdog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Question extends AppCompatActivity {
 
-    private ImageView img_menu, img_bar1, img_bar2, img_bar3, img_bar4, img_bar5, img_bar6;
-    private TextView tv_appname, tv_Q, tv_q1, tv_q2, tv_q3, tv_q4, tv_q5;
+
+    static final String[] LIST_MENU = {"Q. 유기견을 발견하면 어떻게 해야 하나요?", "Q. 보호소 연결이 안 될 경우엔 어떡하죠?", "Q. 유기견 입양하고 싶은데 절차가 어떻게 되나요?",
+            "Q. 비문 등록 잘하는 방법이 궁금해요 !","Q. 비문 등록을 여러개 해놓아도 되나요?"} ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+//        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, LIST_MENU);
 
-        img_menu = findViewById(R.id.img_menu);
-        tv_appname = findViewById(R.id.tv_appname);
-        tv_Q = findViewById(R.id.tv_Q);
-        img_bar1 = findViewById(R.id.img_bar1);
-        img_bar2 = findViewById(R.id.img_bar2);
-        img_bar3 = findViewById(R.id.img_bar3);
-        img_bar4 = findViewById(R.id.img_bar4);
-        img_bar5 = findViewById(R.id.img_bar5);
-        img_bar6 = findViewById(R.id.img_bar6);
-        tv_q1 = findViewById(R.id.tv_q1);
-        tv_q2 = findViewById(R.id.tv_q2);
-        tv_q3 = findViewById(R.id.tv_q3);
-        tv_q4 = findViewById(R.id.tv_q4);
-        tv_q5 = findViewById(R.id.tv_q5);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // 코드 계속 ...
+
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position) ;
+                Log.v("test", strText.toString());
+                Intent intent = new Intent(getApplicationContext(), Question_Result.class);
+                intent.putExtra("title", strText);
+                startActivity(intent);
+
+
+            }
+        }) ;
+
+
+
+//        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String strText = (String) parent.getItemAtPosition(position);
+//                Log.v("test", strText.toString());
+//                Intent intent = new Intent(getApplicationContext(), Question_Result.class);
+//                intent.putExtra("title", strText);
+//
+//                startActivity(intent);
+//            }
+//        });
+
 
     }
 }
--- app/src/main/java/com/example/findyourdog/Question_Result.java
+++ app/src/main/java/com/example/findyourdog/Question_Result.java
@@ -1,25 +1,53 @@
 package com.example.findyourdog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Question_Result extends AppCompatActivity {
 
-    private TextView app_name5, tv_Q, tv_Question, tv_bar7, tv_A1, tv_A2, tv_A3;
+    private TextView tv_question,tv_question_re,tv_question_result,tv_question_reuslt_re;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_result);
 
-        app_name5 = findViewById(R.id.app_name5);
-        tv_Q = findViewById(R.id.tv_Q);
-        tv_Question = findViewById(R.id.tv_Question);
-        tv_bar7 = findViewById(R.id.tv_bar7);
-        tv_A1 = findViewById(R.id.tv_A1);
-        tv_A2 = findViewById(R.id.tv_A2);
-        tv_A3 = findViewById(R.id.tv_A3);
+        tv_question = findViewById(R.id.tv_question);
+        tv_question_re = findViewById(R.id.tv_question_re);
+        tv_question_result = findViewById(R.id.tv_question_result);
+        tv_question_reuslt_re = findViewById(R.id.tv_question_reuslt_re);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+
+        tv_question.setText(title);
+        if (title.equals("Q. 유기견을 발견하면 어떻게 해야 하나요?")){
+            tv_question_re.setText("");
+            tv_question_result.setText("");
+            tv_question_reuslt_re.setText("");
+        }else if(title.equals("Q. 보호소 연결이 안 될 경우엔 어떡하죠?")){
+            tv_question_re.setText("");
+            tv_question_result.setText("");
+            tv_question_reuslt_re.setText("");
+        }else if(title.equals("Q. 유기견 입양하고 싶은데 절차가 어떻게 되나요?")){
+            tv_question_re.setText("");
+            tv_question_result.setText("");
+            tv_question_reuslt_re.setText("");
+        }else if(title.equals("Q. 비문 등록 잘하는 방법이 궁금해요 !")){
+            tv_question_re.setText("");
+            tv_question_result.setText("");
+            tv_question_reuslt_re.setText("");
+        }else if(title.equals("Q. 비문 등록을 여러개 해놓아도 되나요?")){
+            tv_question_re.setText("");
+            tv_question_result.setText("");
+            tv_question_reuslt_re.setText("");
+        }
+        Log.v("title",title);
     }
+
+
 }
