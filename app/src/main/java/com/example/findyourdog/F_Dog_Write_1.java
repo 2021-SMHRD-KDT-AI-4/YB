b7e6eabbd2032bf06b791dd05ad345d5af848373
muyaho <lch980628@gmail.com>
2021-08-07 09:25:17 +0900

    발견했어요 게시글 작성 1페이지 수정

--- app/src/main/java/com/example/findyourdog/F_Dog_Write_1.java
+++ app/src/main/java/com/example/findyourdog/F_Dog_Write_1.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +17,38 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class F_Dog_Write_1 extends AppCompatActivity {
 
     private TextView app_name4, tv_info, tv_tel, tv_date, tv_where, tv_img, tv_time;
     private TextView tv_star6, tv_star7, tv_star8, tv_star9, tv_star10;
-    private Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_f_picture_plus, btn_ok, btn_f_camera;
+    private Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_f_picture_plus, btn_f_next, btn_f_camera;
     private EditText edt_find, edt_tel, edt_where, edt_time;
     private ImageView img_f_picture;
+    private StringRequest stringRequest;
+    private RequestQueue queue;
     File file;
 
     @Override
@@ -47,7 +68,6 @@ public class F_Dog_Write_1 extends AppCompatActivity {
         btn_4 = findViewById(R.id.btn_4);
         btn_5 = findViewById(R.id.btn_5);
         btn_f_picture_plus = findViewById(R.id.btn_f_picture_plus);
-        btn_ok = findViewById(R.id.btn_f_next);
         edt_find = findViewById(R.id.edt_find);
         edt_tel = findViewById(R.id.edt_place);
         edt_where = findViewById(R.id.edt_time);
@@ -60,6 +80,14 @@ public class F_Dog_Write_1 extends AppCompatActivity {
         tv_star10 = findViewById(R.id.tv_star10);
         img_f_picture = findViewById(R.id.img_f_picture);
         btn_f_camera = findViewById(R.id.btn_f_camera);
+        btn_f_next = findViewById(R.id.btn_f_next);
+
+        btn_f_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         btn_f_camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,8 +151,111 @@ public class F_Dog_Write_1 extends AppCompatActivity {
         } else if(requestCode == 1111 && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Log.v("picture",extras.get("data")+"");
             img_f_picture.setImageBitmap(imageBitmap);
+            byte[] img_ =  bitmapToByteArray(imageBitmap);
+            for (int i =0; i<img_.length; i++){
+                Log.v("picture",img_[i]+"");
+            }
+
+
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+//            byte[] byteArray = byteArrayOutputStream .toByteArray();
+//            for (int i=0; i<byteArray.length; i++){
+//                Log.v("picture",byteArray[i]+"");
+//            }
+
         }
 
     }
+//    public void sendRequest(){
+//        // Voolley Lib 새료운 요청객체 생성
+//        queue = Volley.newRequestQueue(getApplicationContext());
+//        String url = "http://211.227.224.206:8081/YB_Project/Login";
+//        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+//            // 응답데이터를 받아오는 곳
+//            @Override
+//            public void onResponse(String response) {
+//                Log.v("resultValue",response);
+//
+//                try {
+//                    JSONObject jsonObject = new JSONObject(response);
+//                    Log.v("resultValue",response);
+//
+//                    String id = jsonObject.getString("id");
+//                    String pw = jsonObject.getString("pw");
+//                    String name = jsonObject.getString("name");
+//                    int membercode = jsonObject.getInt("membercode");
+//                    int tel = jsonObject.getInt("tel");
+//                    String address = jsonObject.getString("address");
+//
+//                    MemberDTO dto = new MemberDTO(id,pw,name,membercode,tel,address);
+//
+//
+//                    Log.v("resultValue", String.valueOf(dto));
+//                    if (id.equals(edt_login_id.getText().toString())){
+//
+//                        PreferenceManager.setString(getApplicationContext(),"id",dto.getId());
+//
+//                        Intent intent = new Intent(getApplicationContext(),Main.class);
+//                        startActivity(intent);
+//
+//                        Log.v("resultValue","로그인성공");
+//                    }else{
+//                        Toast.makeText(getApplicationContext(),"로그인 실패",Toast.LENGTH_SHORT).show();
+//                        Log.v("resultValue","로그인실패");
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            // 서버와의 연동 에러시 출력
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                error.printStackTrace();
+//            }
+//        }){
+//            @Override //response를 UTF8로 변경해주는 소스코드
+//            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+//                try {
+//                    String utf8String = new String(response.data, "UTF-8");
+//                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+//                } catch (UnsupportedEncodingException e) {
+//                    // log error
+//                    return Response.error(new ParseError(e));
+//                } catch (Exception e) {
+//                    // log error
+//                    return Response.error(new ParseError(e));
+//                }
+//            }
+//
+//            // 보낼 데이터를 저장하는 곳
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<>();
+//
+//
+//                params.put("login_id", edt_login_id.getText().toString());
+//                params.put("login_pw", edt_login_pw.getText().toString());
+//
+//                Log.v("login_id",edt_login_id.getText().toString());
+//                Log.v("login_pw",edt_login_pw.getText().toString());
+//
+//
+//                return params;
+//            }
+//        };
+//
+//        queue.add(stringRequest);
+//    }
+public byte[] bitmapToByteArray( Bitmap bitmap ) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream() ;
+        bitmap.compress( Bitmap.CompressFormat.JPEG, 100, stream) ;
+        byte[] byteArray = stream.toByteArray() ;
+        return byteArray ;
+    }
+
 }
