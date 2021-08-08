9c028f78745d330c5c927c600777672d13927483
soyeon <soyeon475211@gmail.com>
2021-08-08 15:24:42 +0900

    입양하기페이지+클릭시 상세보기페이지 대략적인 틀!

--- app/src/main/java/com/example/findyourdog/P_Dog_Detail.java
+++ app/src/main/java/com/example/findyourdog/P_Dog_Detail.java
@@ -9,25 +9,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class P_Dog_Detail extends AppCompatActivity {
 
-    private ImageView img_menu, img_p_dog_detail;
-    private Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_shelter_place, btn_ask;
-    private TextView tv_appname, tv_p_dog_detail;
+
+    private Button  btn_shelter_place, btn_ask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_dog_detail);
 
-        img_menu = findViewById(R.id.img_menu);
-        btn_1 = findViewById(R.id.btn_1);
-        btn_2 = findViewById(R.id.btn_2);
-        btn_3 = findViewById(R.id.btn_3);
-        btn_4 = findViewById(R.id.btn_4);
-        btn_5 = findViewById(R.id.btn_5);
-        img_p_dog_detail = findViewById(R.id.img_p_dog_detail);
+
         btn_shelter_place = findViewById(R.id.btn_shelter_place);
         btn_ask = findViewById(R.id.btn_ask);
-        tv_appname = findViewById(R.id.tv_appname);
-        tv_p_dog_detail = findViewById(R.id.tv_p_dog_detail);
+
     }
 }
--- app/src/main/java/com/example/findyourdog/P_Dog_list.java
+++ app/src/main/java/com/example/findyourdog/P_Dog_list.java
@@ -1,41 +1,35 @@
 package com.example.findyourdog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class P_Dog_list extends Fragment {
 
-    private ImageView img_menu, img_p_dog1, img_p_dog2, img_p_dog3, img_p_dog4;
-    private Button btn_1, btn_2, btn_3, btn_4, btn_5;
-    private TextView tv_appname, tv_p_dog1, tv_p_dog2, tv_p_dog3, tv_p_dog4;
+    private TextView tv_page15,tv_page16,tv_page17,tv_page18,tv_page19;
+    private ImageButton imgbtn_a_write;
+    private GridView gv_adopt;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragment = inflater.inflate(R.layout.activity_p_dog_list, container, false);
 
-        img_menu = fragment.findViewById(R.id.img_menu);
-        btn_1 = fragment.findViewById(R.id.btn_1);
-        btn_2 = fragment.findViewById(R.id.btn_2);
-        btn_3 = fragment.findViewById(R.id.btn_3);
-        btn_4 = fragment.findViewById(R.id.btn_4);
-        btn_5 = fragment.findViewById(R.id.btn_5);
-        img_p_dog1 = fragment.findViewById(R.id.img_p_dog1);
-        img_p_dog2 = fragment.findViewById(R.id.img_p_dog2);
-        img_p_dog3 = fragment.findViewById(R.id.img_p_dog3);
-        img_p_dog4 = fragment.findViewById(R.id.img_p_dog4);
-        tv_appname = fragment.findViewById(R.id.tv_appname);
-        tv_p_dog1 = fragment.findViewById(R.id.tv_p_dog1);
-        tv_p_dog2 = fragment.findViewById(R.id.tv_p_dog2);
-        tv_p_dog3 = fragment.findViewById(R.id.tv_p_dog3);
-        tv_p_dog4 = fragment.findViewById(R.id.tv_p_dog4);
+        tv_page15 = fragment.findViewById(R.id.tv_page15);
+        tv_page16 = fragment.findViewById(R.id.tv_page16);
+        tv_page17 = fragment.findViewById(R.id.tv_page17);
+        tv_page18 = fragment.findViewById(R.id.tv_page18);
+        tv_page19 = fragment.findViewById(R.id.tv_page19);
+        gv_adopt = fragment.findViewById(R.id.gv_adopt);
+
+
+
 
         return fragment;
     }
