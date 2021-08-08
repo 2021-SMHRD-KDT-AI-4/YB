f985d92d39a6685a3099f37424969d4154d82998
muyaho <lch980628@gmail.com>
2021-08-08 22:45:10 +0900

    날짜, 시간 선택가능

--- app/src/main/java/com/example/findyourdog/F_Dog_Write_1.java
+++ app/src/main/java/com/example/findyourdog/F_Dog_Write_1.java
@@ -1,5 +1,7 @@
 package com.example.findyourdog;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,9 +10,11 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -20,6 +24,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class F_Dog_Write_1 extends AppCompatActivity {
 
@@ -28,7 +35,19 @@ public class F_Dog_Write_1 extends AppCompatActivity {
     private Button btn_f_picture_plus, btn_f_next, btn_f_camera;
     private EditText edt_f_day, edt_f_tel, edt_f_place, edt_f_time;
     private ImageView img_f_picture;
+    Calendar myCalendar = Calendar.getInstance();
     File file;
+
+    // 날짜 구하는 거거
+   DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +63,42 @@ public class F_Dog_Write_1 extends AppCompatActivity {
         img_f_picture = findViewById(R.id.img_f_picture);
         btn_f_picture_plus = findViewById(R.id.btn_f_picture_plus);
         btn_f_camera = findViewById(R.id.btn_f_camera);
+
+        // 버튼 누르면 날짜 켈린더 나오고 확인하면 날짜 생성
+        edt_f_day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(F_Dog_Write_1.this, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        // 시간 선택 다이얼로 넘어감감
+       edt_f_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(F_Dog_Write_1.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String state = "AM";
+                        // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
+                        if (selectedHour > 12) {
+                            selectedHour -= 12;
+                            state = "PM";
+                        }
+                        // EditText에 출력할 형식 지정
+                        edt_f_time.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
+                    }
+                }, hour, minute,  true); // true의 경우 24시간 형식의 TimePicker 출현
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+
+
 
         btn_f_next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,5 +173,12 @@ public class F_Dog_Write_1 extends AppCompatActivity {
             img_f_picture.setImageBitmap(imageBitmap);
         }
 
+    }
+    private void updateLabel() {
+        String myFormat = "yyyy/MM/dd";    // 출력형식   2018/11/28
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+        EditText et_date = (EditText) findViewById(R.id.edt_f_day);
+        et_date.setText(sdf.format(myCalendar.getTime()));
     }
 }
